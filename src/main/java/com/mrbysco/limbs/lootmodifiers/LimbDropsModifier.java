f0ec79627bcab4d0e3a3de36aa79e64cad9118c0
Mrbysco <Mrbysco@users.noreply.github.com>
2024-08-07 16:04:05 +0200

    Update LimbDropsModifier.java

--- src/main/java/com/mrbysco/limbs/lootmodifiers/LimbDropsModifier.java
+++ src/main/java/com/mrbysco/limbs/lootmodifiers/LimbDropsModifier.java
@@ -2,7 +2,6 @@ package com.mrbysco.limbs.lootmodifiers;
 
 import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
-import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.mrbysco.limbs.config.LimbConfig;
